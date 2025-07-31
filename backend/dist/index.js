9eed3ee14c7c9da65aaa145ddd3fe113bd74d622
Alaric Jenkins <alaricjenkins@outlook.com>
2025-07-30 23:06:26 -0400

    Compiled new ts code to js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 // Import API endpoints
 const newItem_1 = __importDefault(require("./routers/newItem"));
 const removeItem_1 = __importDefault(require("./routers/removeItem"));
@@ -14,6 +15,7 @@ const updateItemAssignedTo_1 = __importDefault(require("./routers/updateItemAssi
 const app = (0, express_1.default)();
 // Add JSON parsing abilities to express
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
 });
